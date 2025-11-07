2240cc90e4b44515537c99ce6ec46c0768b9a9b9
RB0609 <bomma.rakesh081@gmail.com>
2025-11-07 13:25:46 +0100

    update project to latest version

--- build/dd_robot/cmake_install.cmake
+++ build/dd_robot/cmake_install.cmake
@@ -43,7 +43,73 @@ if(NOT DEFINED CMAKE_OBJDUMP)
 endif()
 
 if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  include("/home/rakesh/ros2_ws/build/dd_robot/ament_cmake_symlink_install/ament_cmake_symlink_install.cmake")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/dd_robot/" TYPE DIRECTORY FILES
+    "/home/rakesh/ros2_ws/src/differential_drive/dd_robot/urdf"
+    "/home/rakesh/ros2_ws/src/differential_drive/dd_robot/launch"
+    "/home/rakesh/ros2_ws/src/differential_drive/dd_robot/rviz"
+    "/home/rakesh/ros2_ws/src/differential_drive/dd_robot/config"
+    "/home/rakesh/ros2_ws/src/differential_drive/dd_robot/worlds"
+    "/home/rakesh/ros2_ws/src/differential_drive/dd_robot/scripts"
+    )
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ament_index/resource_index/package_run_dependencies" TYPE FILE FILES "/home/rakesh/ros2_ws/build/dd_robot/ament_cmake_index/share/ament_index/resource_index/package_run_dependencies/dd_robot")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ament_index/resource_index/parent_prefix_path" TYPE FILE FILES "/home/rakesh/ros2_ws/build/dd_robot/ament_cmake_index/share/ament_index/resource_index/parent_prefix_path/dd_robot")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/dd_robot/environment" TYPE FILE FILES "/opt/ros/jazzy/share/ament_cmake_core/cmake/environment_hooks/environment/ament_prefix_path.sh")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/dd_robot/environment" TYPE FILE FILES "/home/rakesh/ros2_ws/build/dd_robot/ament_cmake_environment_hooks/ament_prefix_path.dsv")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/dd_robot/environment" TYPE FILE FILES "/opt/ros/jazzy/share/ament_cmake_core/cmake/environment_hooks/environment/path.sh")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/dd_robot/environment" TYPE FILE FILES "/home/rakesh/ros2_ws/build/dd_robot/ament_cmake_environment_hooks/path.dsv")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/dd_robot" TYPE FILE FILES "/home/rakesh/ros2_ws/build/dd_robot/ament_cmake_environment_hooks/local_setup.bash")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/dd_robot" TYPE FILE FILES "/home/rakesh/ros2_ws/build/dd_robot/ament_cmake_environment_hooks/local_setup.sh")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/dd_robot" TYPE FILE FILES "/home/rakesh/ros2_ws/build/dd_robot/ament_cmake_environment_hooks/local_setup.zsh")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/dd_robot" TYPE FILE FILES "/home/rakesh/ros2_ws/build/dd_robot/ament_cmake_environment_hooks/local_setup.dsv")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/dd_robot" TYPE FILE FILES "/home/rakesh/ros2_ws/build/dd_robot/ament_cmake_environment_hooks/package.dsv")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ament_index/resource_index/packages" TYPE FILE FILES "/home/rakesh/ros2_ws/build/dd_robot/ament_cmake_index/share/ament_index/resource_index/packages/dd_robot")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/dd_robot/cmake" TYPE FILE FILES
+    "/home/rakesh/ros2_ws/build/dd_robot/ament_cmake_core/dd_robotConfig.cmake"
+    "/home/rakesh/ros2_ws/build/dd_robot/ament_cmake_core/dd_robotConfig-version.cmake"
+    )
+endif()
+
+if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/dd_robot" TYPE FILE FILES "/home/rakesh/ros2_ws/src/differential_drive/dd_robot/package.xml")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
